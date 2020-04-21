3b3af1c835e2c32ced9f7aee36748267e55a2767
Matt <subbassbro@gmail.com>
2020-04-21 01:13:54 -0400

    Added Falcon-256. The Parameter on the detached signature / Shared secret needs to be resolved, but everything else should be working

--- pqcrypto-falcon/src/ffi.rs
+++ pqcrypto-falcon/src/ffi.rs
@@ -3,18 +3,55 @@
 //! This module defines the foreign function interface for the following
 //! crypto implementations from PQClean:
 //!
+//!  * falcon-256
 //!  * falcon-512
 //!  * falcon-1024
 // This file has been generated from PQClean.
 // Find the templates in pqcrypto-template
 use libc::c_int;
 
+pub const PQCLEAN_FALCON256_CLEAN_CRYPTO_SECRETKEYBYTES: usize = 641;
+pub const PQCLEAN_FALCON256_CLEAN_CRYPTO_PUBLICKEYBYTES: usize = 449;
+pub const PQCLEAN_FALCON256_CLEAN_CRYPTO_BYTES: usize = 362; // I had to guess here, so don't take this number as correct
 pub const PQCLEAN_FALCON512_CLEAN_CRYPTO_SECRETKEYBYTES: usize = 1281;
 pub const PQCLEAN_FALCON512_CLEAN_CRYPTO_PUBLICKEYBYTES: usize = 897;
 pub const PQCLEAN_FALCON512_CLEAN_CRYPTO_BYTES: usize = 690;
 pub const PQCLEAN_FALCON1024_CLEAN_CRYPTO_SECRETKEYBYTES: usize = 2305;
 pub const PQCLEAN_FALCON1024_CLEAN_CRYPTO_PUBLICKEYBYTES: usize = 1793;
 pub const PQCLEAN_FALCON1024_CLEAN_CRYPTO_BYTES: usize = 1330;
+
+#[link(name = "falcon-256_clean")]
+extern "C" {
+    pub fn PQCLEAN_FALCON256_CLEAN_crypto_sign_keypair(pk: *mut u8, sk: *mut u8) -> c_int;
+    pub fn PQCLEAN_FALCON256_CLEAN_crypto_sign(
+        sm: *mut u8,
+        smlen: *mut usize,
+        msg: *const u8,
+        len: usize,
+        sk: *const u8,
+    ) -> c_int;
+    pub fn PQCLEAN_FALCON256_CLEAN_crypto_sign_open(
+        m: *mut u8,
+        mlen: *mut usize,
+        sm: *const u8,
+        smlen: usize,
+        pk: *const u8,
+    ) -> c_int;
+    pub fn PQCLEAN_FALCON256_CLEAN_crypto_sign_signature(
+        sig: *mut u8,
+        siglen: *mut usize,
+        m: *const u8,
+        mlen: usize,
+        sk: *const u8,
+    ) -> c_int;
+    pub fn PQCLEAN_FALCON256_CLEAN_crypto_sign_verify(
+        sig: *const u8,
+        siglen: usize,
+        m: *const u8,
+        mlen: usize,
+        pk: *const u8,
+    ) -> c_int;
+}
 
 #[link(name = "falcon-512_clean")]
 extern "C" {
@@ -80,6 +117,120 @@ extern "C" {
         mlen: usize,
         pk: *const u8,
     ) -> c_int;
+}
+
+#[cfg(test)]
+mod test_falcon256_clean {
+    use super::*;
+    use rand::prelude::*;
+
+    #[test]
+    fn test_ffi() {
+        unsafe {
+            let mut rng = rand::thread_rng();
+            let mut mlen: usize = rng.gen::<u16>() as usize;
+            let msg: Vec<u8> = (0..mlen).map(|_| rng.gen()).collect();
+
+            let mut pk = vec![0u8; PQCLEAN_FALCON256_CLEAN_CRYPTO_PUBLICKEYBYTES];
+            let mut sk = vec![0u8; PQCLEAN_FALCON256_CLEAN_CRYPTO_SECRETKEYBYTES];
+            let mut pk_alt = vec![0u8; PQCLEAN_FALCON256_CLEAN_CRYPTO_PUBLICKEYBYTES];
+            let mut sk_alt = vec![0u8; PQCLEAN_FALCON256_CLEAN_CRYPTO_SECRETKEYBYTES];
+            let mut detached_sig = vec![0u8; PQCLEAN_FALCON256_CLEAN_CRYPTO_BYTES];
+            let mut sm = Vec::with_capacity(mlen + PQCLEAN_FALCON256_CLEAN_CRYPTO_BYTES);
+            let mut smlen = 0;
+            assert_eq!(
+                0,
+                PQCLEAN_FALCON256_CLEAN_crypto_sign_keypair(pk.as_mut_ptr(), sk.as_mut_ptr())
+            );
+            assert_eq!(
+                0,
+                PQCLEAN_FALCON256_CLEAN_crypto_sign(
+                    sm.as_mut_ptr(),
+                    &mut smlen as *mut usize,
+                    msg.as_ptr(),
+                    mlen,
+                    sk.as_ptr()
+                )
+            );
+            sm.set_len(smlen);
+
+            let mut unpacked_m = Vec::with_capacity(mlen + PQCLEAN_FALCON256_CLEAN_CRYPTO_BYTES);
+            assert_eq!(
+                0,
+                PQCLEAN_FALCON256_CLEAN_crypto_sign_open(
+                    unpacked_m.as_mut_ptr(),
+                    &mut mlen as *mut usize,
+                    sm.as_ptr(),
+                    sm.len(),
+                    pk.as_ptr()
+                )
+            );
+            unpacked_m.set_len(mlen);
+            assert_eq!(unpacked_m, msg);
+
+            // check verification fails with wrong pk
+            assert_eq!(
+                0,
+                PQCLEAN_FALCON256_CLEAN_crypto_sign_keypair(
+                    pk_alt.as_mut_ptr(),
+                    sk_alt.as_mut_ptr()
+                )
+            );
+            assert!(
+                PQCLEAN_FALCON256_CLEAN_crypto_sign_open(
+                    unpacked_m.as_mut_ptr(),
+                    &mut mlen as *mut usize,
+                    sm.as_ptr(),
+                    sm.len(),
+                    pk_alt.as_ptr()
+                ) < 0
+            );
+
+            assert_eq!(
+                0,
+                PQCLEAN_FALCON256_CLEAN_crypto_sign_signature(
+                    detached_sig.as_mut_ptr(),
+                    &mut smlen as *mut usize,
+                    msg.as_ptr(),
+                    msg.len(),
+                    sk.as_ptr()
+                )
+            );
+            assert!(
+                smlen <= PQCLEAN_FALCON256_CLEAN_CRYPTO_BYTES,
+                "Signed message length should be ≤ CRYPTO_BYTES"
+            );
+            assert_eq!(
+                0,
+                PQCLEAN_FALCON256_CLEAN_crypto_sign_verify(
+                    detached_sig.as_ptr(),
+                    smlen,
+                    msg.as_ptr(),
+                    msg.len(),
+                    pk.as_ptr()
+                )
+            );
+            assert!(
+                PQCLEAN_FALCON256_CLEAN_crypto_sign_verify(
+                    detached_sig.as_ptr(),
+                    smlen,
+                    msg.as_ptr(),
+                    msg.len(),
+                    pk_alt.as_ptr()
+                ) < 0
+            );
+
+            assert!(
+                PQCLEAN_FALCON256_CLEAN_crypto_sign_verify(
+                    detached_sig.as_ptr(),
+                    smlen,
+                    msg.as_ptr(),
+                    msg.len() - 1,
+                    pk.as_ptr()
+                ) < 0
+            );
+        }
+    }
 }
 
 #[cfg(test)]
@@ -195,6 +346,7 @@ mod test_falcon512_clean {
         }
     }
 }
+
 #[cfg(test)]
 mod test_falcon1024_clean {
     use super::*;
